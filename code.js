bcd3062409dc743feb90737f7a9e7fee3dc688fc
Dachang Liu <dachang_liu@mlcn000298dliu.local>
2019-08-16 15:11:33 +0800

    finish width & height function

--- code.js
+++ code.js
@@ -10,7 +10,7 @@ figma.loadFontAsync({ family: "Roboto", style: "Regular" });
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
 figma.ui.onmessage = msg => {
-    // create width spec and display at the bottom / center / top of the frame
+    // create width spec and display at the bottom / center / top of the component
     if (msg.type === 'create-width-bottom' ||
         msg.type === 'create-width-top' ||
         msg.type === 'create-width-center') {
@@ -59,30 +59,47 @@ figma.ui.onmessage = msg => {
             widthFrame.appendChild(widthText);
         }
     }
-    // create height spec and display at the right / center / left of the frame
-    if (msg.type === 'create-height-right') {
+    // create height spec and display at the right / center / left of the component
+    if (msg.type === 'create-height-right' ||
+        msg.type === 'create-height-left' ||
+        msg.type === 'create-height-center') {
         for (const node of figma.currentPage.selection) {
             // set up base frame
             const heightFrame = figma.createFrame();
-            heightFrame.resizeWithoutConstraints(node.width + 20, node.height + 34);
             heightFrame.backgrounds = [{ type: 'SOLID', color: { r: 1, g: 1, b: 1 }, opacity: 0 }];
-            heightFrame.x = node.x;
             heightFrame.y = node.y;
             node.parent.appendChild(heightFrame);
             // calculate height & displays
             const heightText = figma.createText();
             heightText.characters = node.height.toString().concat("px");
-            heightText.x = node.width - 8;
             heightText.y = (node.height - heightText.height) / 2;
             heightText.fills = [{ type: 'SOLID', color: { r: 1, g: 1, b: 1 } }];
             const textContainer = figma.createRectangle();
             textContainer.resizeWithoutConstraints(heightText.width, heightText.height);
-            textContainer.x = heightText.x;
             textContainer.y = heightText.y;
             textContainer.fills = [{ type: 'SOLID', color: { r: 1, g: 0.33, b: 0 } }];
             heightFrame.resizeWithoutConstraints(node.width + 11 + heightText.height, node.height);
             const heightLine = figma.createLine();
-            heightLine.x = node.width + 10;
+            if (msg.type === 'create-height-right') {
+                heightFrame.x = node.x;
+                heightText.x = node.width - 8;
+                textContainer.x = heightText.x;
+                heightLine.x = node.width + 10;
+            }
+            else if (msg.type === 'create-height-left') {
+                heightFrame.x = node.x - 20;
+                heightFrame.resizeWithoutConstraints(node.width + 20, node.height);
+                heightText.x = 0;
+                textContainer.x = heightText.x;
+                heightLine.x = 13;
+            }
+            else {
+                heightFrame.x = node.x;
+                heightFrame.resizeWithoutConstraints(node.width, node.height);
+                heightText.x = (node.width - heightText.width) / 2;
+                textContainer.x = heightText.x;
+                heightLine.x = node.width / 2;
+            }
             heightLine.y = 0;
             heightLine.strokes = [{ type: 'SOLID', color: { r: 1, g: 0.33, b: 0 } }];
             heightLine.strokeCap = "ARROW_EQUILATERAL";
@@ -94,6 +111,7 @@ figma.ui.onmessage = msg => {
             heightFrame.appendChild(heightText);
         }
     }
+    // create spacing spec between an component & the outer canvas
     // Make sure to close the plugin when you're done. Otherwise the plugin will
     // keep running, which shows the cancel button at the bottom of the screen.
     if (msg.type === 'cancel') {
